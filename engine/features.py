7da03f121546968adf8dc8b8b9348bb783ef5d36
anuj.bhokse <215415632+anujbhokse10@users.noreply.github.com>
2025-06-10 18:57:51 +0530

    Day 3 complete

---
+++ engine/features.py
@@ -0,0 +1,7 @@
+from playsound import playsound 
+
+
+#playing assistant sound function 
+def playAssistantsound():
+    music_dir = "C:\\Users\\Anuj\\OneDrive\\Desktop\\Jarvis\\www\\assets\\audio\\www_assets_audio_start_sound.mp3"
+    playsound(music_dir)
--- www/main.js
+++ www/main.js
@@ -12,4 +12,29 @@ $(document).ready(function () {
         },
     });
 
+    // Siri configuration
+     var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 800,
+    height: 200,
+    style: "ios9",
+    amplitude: "1",
+    speed: "0.30",
+    autostart: true,
+  });  
+  
+   // Siri Message animation
+     $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true,
+        },
+    });
+
 });
