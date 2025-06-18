fce0915082ab8c874d2017deddbf97704d06d92e
anuj.bhokse <215415632+anujbhokse10@users.noreply.github.com>
2025-06-18 20:49:36 +0530

    Day 10 complete

--- engine/db.py
+++ engine/db.py
@@ -11,12 +11,12 @@ cursor.execute(query)
 # cursor.execute(query)
 # con.commit()
 
-query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
-cursor.execute(query)
+# query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+# cursor.execute(query)
 
-query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
-cursor.execute(query)
-con.commit()
+# query = "INSERT INTO web_command VALUES (null,'youtube', 'https://www.youtube.com/')"
+# cursor.execute(query)
+# con.commit()
 
 
 # testing module
--- engine/features.py
+++ engine/features.py
@@ -1,5 +1,8 @@
 import os
 import re
+import sqlite3
+import webbrowser
+from colorama import cursor
 from playsound import playsound 
 import eel
 
@@ -7,6 +10,9 @@ from engine.command import speak
 from engine.config import ASSISTANT_NAME
 import pywhatkit as kit 
 #playing assistant sound function
+
+con = sqlite3.connect("jarvis.db")
+cursor = con.cursor()
 
 @eel.expose 
 def playAssistantsound():
@@ -19,11 +25,36 @@ def openCommand(query):
     query = query.replace("open", "")
     query.lower()
     
-    if query!= "":
-        speak("opening " + query)
-        os.system("start " + query)
-    else:
-        speak("not found")
+    app_name = query.strip()
+
+    if app_name != "":
+
+        try:
+            cursor.execute(
+                'SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+            results = cursor.fetchall()
+
+            if len(results) != 0:
+                speak("Opening "+query)
+                os.startfile(results[0][0])
+
+            elif len(results) == 0: 
+                cursor.execute(
+                'SELECT url FROM web_command WHERE name IN (?)', (app_name,))
+                results = cursor.fetchall()
+
+                if len(results) != 0:
+                    speak("Opening "+query)
+                    webbrowser.open(results[0][0])
+
+                else:
+                    speak("Opening "+query)
+                    try:
+                        os.system('start '+query)
+                    except:
+                        speak("not found")
+        except:
+            speak("some thing went wrong")
     
     
 def PlayYoutube(query):
